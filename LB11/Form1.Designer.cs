295936db1360222a269eaca132199ecbdaf233fc
Crivetkaaa <crivetkaa@vk.com>
2023-04-21 23:15:52 +0300

    first commit

---
+++ LB11/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace LB11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "F1 = sqrt(x)    F2 = 1/x    F3 = e^x";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "123",
+            "132",
+            "213",
+            "231",
+            "312",
+            "321"});
+            this.comboBox1.Location = new System.Drawing.Point(137, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(75, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(290, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Последовательность:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(218, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Значение x:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(452, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Значение y:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(85, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(371, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Расчёт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 110);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "VBA код:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 126);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(430, 173);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 311);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Комбинирование";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ LB11/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LB11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string val = comboBox1.Text;
+            if (val == "")
+            {
+                label4.Text = "";
+            }
+            else if (val == "123")
+            {
+                label4.Text = "y = F1(F2(F3(x)))";
+            }
+            else if (val == "132")
+            {
+                label4.Text = "y = F1(F3(F2(x)))";
+            }
+            else if (val == "213")
+            {
+                label4.Text = "y = F2(F1(F3(x)))";
+            }
+            else if (val == "231")
+            {
+                label4.Text = "y = F2(F3(F1(x)))";
+            }
+            else if (val == "312")
+            {
+                label4.Text = "y = F3(F1(F2(x)))";
+            }
+            else if (val == "321")
+            {
+                label4.Text = "y = F3(F2(F1(x)))";
+            }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string val = comboBox1.Text;
+            string x = textBox1.Text;
+            if (val == "" || x == "")
+            {
+                MessageBox.Show("Введите данные");
+            }
+            else
+            {
+                if (val == "123")
+                {
+                    double y1 = Math.Pow(Math.E, Double.Parse(x));
+                    double y2 = 1 / y1;
+                    double y3 = Math.Sqrt(y2);
+                    if (y3.ToString() == "∞" || Double.IsNaN(y3) == true)
+                    {
+                        MessageBox.Show("Вне области определения");
+                    }
+                    else
+                    {
+                        label6.Text = y3.ToString();
+                    }
+                    richTextBox1.Text = "Function F(x As Double)  '= Math.Sqrt(1/Exp(x))\r\nOn Error Go To E_Catch\r\n  F = Math.Sqrt(1/Exp(x))\r\n  Exit Function\r\nE_Catch:\r\n  Select Case Err.Number\r\n  Case 11\r\n    Call MsgBox(\"Division by 0\",, \"Error)\r\n  Case 5\r\n    Call MsgBox(\"Square root of negative number\",, \"Error\")\r\n  Case Else\r\n    Call MsgBox(\"Error: \" & Err.Number & \"\" & Err.Description)\r\n  End Select\r\n  Err.Raise Err.Number\r\nEnd Function\r\n\r\nSub TestF()\r\nDim x As Double\r\nx = Application.InputBox()\r\nDebug.Print(F(x))\r\nEnd Sub";
+                }
+                else if (val == "132")
+                {
+                    double y1 = 1 / Double.Parse(x);
+                    double y2 = Math.Pow(Math.E, y1);
+                    double y3 = Math.Sqrt(y2);
+                    if (y3.ToString() == "∞" || Double.IsNaN(y3) == true)
+                    {
+                        MessageBox.Show("Вне области определения");
+                    }
+                    else
+                    {
+                        label6.Text = y3.ToString();
+                    }
+                    richTextBox1.Text = "Function F(x As Double)  '= Math.Sqrt(Exp(1/x))\r\nOn Error Go To E_Catch\r\n  F = Math.Sqrt(Exp(1/x))\r\n  Exit Function\r\nE_Catch:\r\n  Select Case Err.Number\r\n  Case 11\r\n    Call MsgBox(\"Division by 0\",, \"Error)\r\n  Case 5\r\n    Call MsgBox(\"Square root of negative number\",, \"Error\")\r\n  Case Else\r\n    Call MsgBox(\"Error: \" & Err.Number & \"\" & Err.Description)\r\n  End Select\r\n  Err.Raise Err.Number\r\nEnd Function\r\n\r\nSub TestF()\r\nDim x As Double\r\nx = Application.InputBox()\r\nDebug.Print(F(x))\r\nEnd Sub";
+                }
+                else if (val == "213")
+                {
+                    double y1 = Math.Pow(Math.E, Double.Parse(x));
+                    double y2 = Math.Sqrt(y1);
+                    double y3 = 1 / y2;
+                    if (y3.ToString() == "∞" || Double.IsNaN(y3) == true)
+                    {
+                        MessageBox.Show("Вне области определения");
+                    }
+                    else
+                    {
+                        label6.Text = y3.ToString();
+                    }
+                    richTextBox1.Text = "Function F(x As Double)  '= 1/(Math.Sqrt(Exp(x))\r\nOn Error Go To E_Catch\r\n  F = 1/(Math.Sqrt(Exp(x))\r\n  Exit Function\r\nE_Catch:\r\n  Select Case Err.Number\r\n  Case 11\r\n    Call MsgBox(\"Division by 0\",, \"Error)\r\n  Case 5\r\n    Call MsgBox(\"Square root of negative number\",, \"Error\")\r\n  Case Else\r\n    Call MsgBox(\"Error: \" & Err.Number & \"\" & Err.Description)\r\n  End Select\r\n  Err.Raise Err.Number\r\nEnd Function\r\n\r\nSub TestF()\r\nDim x As Double\r\nx = Application.InputBox()\r\nDebug.Print(F(x))\r\nEnd Sub";
+                }
+                else if (val == "231")
+                {
+                    double y1 = Math.Sqrt(Double.Parse(x));
+                    double y2 = Math.Pow(Math.E, y1);
+                    double y3 = 1 / y2;
+                    if (y3.ToString() == "∞" || Double.IsNaN(y3) == true)
+                    {
+                        MessageBox.Show("Вне области определения");
+                    }
+                    else
+                    {
+                        label6.Text = y3.ToString();
+                    }
+                    richTextBox1.Text = "Function F(x As Double)  '= 1/(Exp(Math.Sqrt(x))\r\nOn Error Go To E_Catch\r\n  F = 1/(Exp(Math.Sqrt(x))\r\n  Exit Function\r\nE_Catch:\r\n  Select Case Err.Number\r\n  Case 11\r\n    Call MsgBox(\"Division by 0\",, \"Error)\r\n  Case 5\r\n    Call MsgBox(\"Square root of negative number\",, \"Error\")\r\n  Case Else\r\n    Call MsgBox(\"Error: \" & Err.Number & \"\" & Err.Description)\r\n  End Select\r\n  Err.Raise Err.Number\r\nEnd Function\r\n\r\nSub TestF()\r\nDim x As Double\r\nx = Application.InputBox()\r\nDebug.Print(F(x))\r\nEnd Sub";
+                }
+                else if (val == "312")
+                {
+                    double y1 = 1 / Double.Parse(x);
+                    double y2 = Math.Sqrt(y1);
+                    double y3 = Math.Pow(Math.E, y2);
+                    if (y3.ToString() == "∞" || Double.IsNaN(y3) == true)
+                    {
+                        MessageBox.Show("Вне области определения");
+                    }
+                    else
+                    {
+                        label6.Text = y3.ToString();
+                    }
+                    richTextBox1.Text = "Function F(x As Double)  '= Exp(Math.Sqrt(1/x))\r\nOn Error Go To E_Catch\r\n  F = Exp(Math.Sqrt(1/x))\r\n  Exit Function\r\nE_Catch:\r\n  Select Case Err.Number\r\n  Case 11\r\n    Call MsgBox(\"Division by 0\",, \"Error)\r\n  Case 5\r\n    Call MsgBox(\"Square root of negative number\",, \"Error\")\r\n  Case Else\r\n    Call MsgBox(\"Error: \" & Err.Number & \"\" & Err.Description)\r\n  End Select\r\n  Err.Raise Err.Number\r\nEnd Function\r\n\r\nSub TestF()\r\nDim x As Double\r\nx = Application.InputBox()\r\nDebug.Print(F(x))\r\nEnd Sub";
+                }
+                else if (val == "321")
+                {
+                    double y1 = Math.Sqrt(Double.Parse(x));
+                    double y2 = 1 / y1;
+                    double y3 = Math.Pow(Math.E, y2);
+                    if (y3.ToString() == "∞" || Double.IsNaN(y3) == true)
+                    {
+                        MessageBox.Show("Вне области определения");
+                    }
+                    else
+                    {
+                        label6.Text = y3.ToString();
+                    }
+                    richTextBox1.Text = "Function F(x As Double)  '= Exp(1/Math.Sqrt(x))\r\nOn Error Go To E_Catch\r\n  F = Exp(1/Math.Sqrt(x))\r\n  Exit Function\r\nE_Catch:\r\n  Select Case Err.Number\r\n  Case 11\r\n    Call MsgBox(\"Division by 0\",, \"Error)\r\n  Case 5\r\n    Call MsgBox(\"Square root of negative number\",, \"Error\")\r\n  Case Else\r\n    Call MsgBox(\"Error: \" & Err.Number & \"\" & Err.Description)\r\n  End Select\r\n  Err.Raise Err.Number\r\nEnd Function\r\n\r\nSub TestF()\r\nDim x As Double\r\nx = Application.InputBox()\r\nDebug.Print(F(x))\r\nEnd Sub";
+                }
+            }
+        }
+    }
+}
